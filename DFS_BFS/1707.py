d2eadc10338ea344a7b15ae83c8fd83a64510c36
jinhyuck <gjwlsgur4866@gmail.com>
2022-03-25 18:24:28 +0900

    DFS_BFS Day-10

--- DFS_BFS/1707.py
+++ DFS_BFS/1707.py
@@ -2,51 +2,57 @@
 # 그래프의 정점의 집합을 둘로 분할하여, 각 집합에 속한 정점끼리는 서로 인접하지 않도록 분할할 수 있을 때
 # print는 리턴 값이 없음
 from collections import deque
-from tokenize import group
+import sys
 
 
 def bfs(go):
     que = deque()
     que.append(go)
     visited[go] = 1
-    g[go] = 1
+    # print(data)
 
     while que:
-        print(que, 'v', visited)
+        print('이전', que, 'v', visited)
         node = que.popleft()
+        # print('node', node)
+        print('이후', que, 'v', visited)
         for next in data[node]:
             if visited[next] == 0:
-                visited[next] = 1
-                if g[node] == 1:
-                    g[next] = 2
-                elif g[node] == 2:
-                    g[next] = 1
+                que.append(next)
+                print('n', next)
+                if visited[node] == 1:
+                    visited[next] = 2
+                    # que.append(next)
+                if visited[node] == 2:
+                    visited[next] = 1
+                    # que.append(next)
             elif visited[node] == visited[next]:
-                if g[node] == g[next]:
-                    print("거의 다왔쪄요")
-                    return False
-                else:
-                    return True
+                # print("거의 다왔음")
+                return False
 
-ans = []       
+    return True
+
 k = int(input())    # 케이스 수
 for _ in range(k):
-    v, e = map(int, input().split())    # v는 노드 / e는 간선
+    v, e = map(int, sys.stdin.readline().split())    # v는 노드 / e는 간선
     data = [[] for _ in range(v+1)]
-    visited = [0] * (v+1)   # 0 방문없음 / 1 방문했쨔냐
-    g = [0] * (v+1) # 그룹1/ 그룹2
+    visited = [0] * (v+1)   # 0 방문없음 / 그룹 1 / 그룹 2
     
-
     for _ in range(e):
-        a, b = map(int, input().split())
+        a, b = map(int, sys.stdin.readline().split())
         data[a].append(b)
         data[b].append(a)
-    
+
+    ans = True
     for i in range(1, v+1):
         if visited[i] == 0:
-            bfs(i)
+            # print('start', i)
+            ans = bfs(i)
+            if not ans:
+                break
 
-for _ in range(k):
-    ans.append(bfs(i))
-print(ans)
+    if ans:
+        print('YES')
+    else:
+        print('NO')
 
