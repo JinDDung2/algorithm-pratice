a2c8bae568eb91cd1f5b7c38e1f96b2556cb6057
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-09 20:45:30 +0900

    베스트앨범 수정

--- programmas/lv2/bestalbum.py
+++ programmas/lv2/bestalbum.py
@@ -1,32 +1,30 @@
-from operator import ge
-from re import T
-import re
-
-
 def solution(genres, plays):
     answer = []
     total_dict = {} # 장르마다 플레이 수
     per_dict = {} # 장르 안의 각 노래마다 플레이 수
 
-    
+    # 딕셔너리 안에 찾으려는 Key 값이 없을 경우 미리 정해 둔 디폴트 값을 대신 가져오게 하고 싶을 때에는 get(x, '디폴트 값')을 사용하면 된다!!
+
     for i in range(len(genres)):
         total_dict[genres[i]] = total_dict.get(genres[i], 0) + plays[i]
         per_dict[genres[i]] = per_dict.get(genres[i], []) + [(plays[i], i)]
     
-    # print(f"total_dict:{total_dict}")
-    # print(f"per_dict:{per_dict}")
+    print(f"total_dict:{total_dict}")
+    print(f"per_dict:{per_dict}")
 
-    # 내림차순 정렬
+    # 총 재생횟수 내림차순 정렬
     total_sort = sorted(total_dict.items(), key= lambda x:x[1], reverse=True)
-    per_sort = sorted(per_dict.items(), key= lambda x: (-x[0], x[1]), reverse=True)
     # print(total_sort)
 
-    # 장르마다 2개 선택
-    first_genre = total_sort[0][0]
-    second_genre = total_sort[1][0]
-    # print(per_dict.items())
-    print(per_sort)
-    
+    for genre in per_dict:
+        # print(per_dict[genre])
+        per_dict[genre] = sorted(per_dict[genre], key= lambda x: (-x[0]))
+
+    for (genre, totalPlay) in total_sort:
+        print(per_dict[genre])
+        for (plays, idx) in per_dict[genre][:2]:
+            answer.append(idx)
+            
     return answer
 
 genres = ["classic", "pop", "classic", "classic", "pop"]
