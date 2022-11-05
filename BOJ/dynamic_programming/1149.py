3c64e42602e162081a4340e55bb988e3d048faa3
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-05 20:49:49 +0900

    RGB 거리

--- BOJ/dynamic_programming/1149.py
+++ BOJ/dynamic_programming/1149.py
@@ -1,10 +1,26 @@
 # RGB거리
 import sys
 
-data = []
+dp = []
+# [[26, 40, 83], [49, 60, 57], [13, 89, 99]] 
+#      빨강             초록            파랑
 n = int(sys.stdin.readline())
-dp = [0] * (n+1)
+
 for _ in range(n):
     cost = list(map(int, sys.stdin.readline().split()))
-    data.append(cost)
+    dp.append(cost)
+# print(data)
+
+# N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+for i in range(1, n):
+    # red
+    dp[i][0] = dp[i][0] + min(dp[i-1][1], dp[i-1][2])
+    # green
+    dp[i][1] = dp[i][1] + min(dp[i-1][0], dp[i-1][2])
+    # blue
+    dp[i][2] = dp[i][2] + min(dp[i-1][0], dp[i-1][1])
+
+print(min(dp[n-1]))
+print(dp)
+
 
