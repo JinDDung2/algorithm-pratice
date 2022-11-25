bf328b2b73b4942384f52a29820d9b4d04d55a7f
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-25 18:12:23 +0900

    정렬 제거

--- programmas/lv1/budget.py
+++ programmas/lv1/budget.py
@@ -1,10 +1,8 @@
 # 예산
 def solution(d, budget):
-    d_sort = sorted(d)
-    # print(d_sort)
-    while sum(d_sort) > budget:
-        d_sort.pop()
-        # print(d_sort)
-    answer = len(d_sort)
+    while sum(d) > budget:
+        d.pop()
+        # print(d)
+    answer = len(d)
 
     return answer
