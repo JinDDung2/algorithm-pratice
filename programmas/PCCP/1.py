9b36942808ff1049fd8a778fabf3a9b4b93740fc
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-17 14:30:03 +0900

    [PCCP 모의고사 #1] 외톨이 알파벳

---
+++ programmas/PCCP/1.py
@@ -0,0 +1,38 @@
+# [PCCP 모의고사 #1] 외톨이 알파벳
+
+'''
+input_string	result
+"edeaaabbccd"	"de"
+"eeddee"	"e"
+"string"	"N"
+"zbzbz"	"bz"
+'''
+
+def solution(input_string):
+    dict = {}
+    numIdx = []
+    
+    for idx, char in enumerate(input_string):
+        if char not in dict:
+            dict[char] = [idx]
+        else:
+            dict[char].append(idx)
+    # print(dict)
+    
+    answer_list = []
+    for char, value in dict.items():
+        # print(char, value)
+        if len(value) >= 2:
+            for i in range(len(value)-1):
+                if value[i+1] - value[i] > 1:
+                    answer_list.append(char)
+                    break
+    
+    answer_list.sort()
+    if answer_list:
+        answer = ''.join(answer_list)
+    else:
+        answer = 'N'
+    return answer
+
+
