f2d2f86e397b10f2b68205e4900089100d8f0346
Jin-hyuck <gjwlsgur4866@gmail.com>
2022-12-26 21:15:15 +0900

    2018 KAKAO BLIND RECRUITMENT [1차] 캐시

--- programmas/lv2/cache.py
+++ programmas/lv2/cache.py
@@ -1,3 +1,24 @@
 # 2018 KAKAO BLIND RECRUITMENT [1차] 캐시
 from collections import deque
 
+def solution(cacheSize, cities):
+    time = 0
+    
+    que = deque([])
+    for city in cities:
+        city = city.lower()
+        
+        if city in que:
+            que.remove(city)
+            que.append(city)
+            time += 1
+        else:
+            time += 5
+            
+            if cacheSize == 0:
+                continue
+                
+            if len(que) >= cacheSize:
+                que.popleft()
+            que.append(city)
+    return time
