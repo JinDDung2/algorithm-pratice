7c7d481ef8e8f78a3da21261d4ec696216f19347
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-03-09 10:48:58 +0900

    [lv3] 단어변환 bfs

--- programmas/lv3/change_word.py
+++ programmas/lv3/change_word.py
@@ -17,3 +17,31 @@ def incorrect_solution(begin, target, words):
                 
     return answer
 
+# 단어 변환을 할 필요가 없음 -> 다음 단어가 변환된 단어이기 때문
+from collections import deque
+
+def solution(begin, target, words):
+    answer = 0
+    
+    if target not in words:
+        return answer
+    
+    q = deque()
+    q.append([begin, 0])
+    # 두 단어가 1글자만 달라야함
+    while q:
+        temp, cnt = q.popleft()
+        
+        if temp == target:
+            answer = cnt
+            break
+        
+        for k in range(len(words)):
+            temp_cnt = 0
+            for i in range(len(temp)):
+                if temp[i] != words[k][i]:
+                    temp_cnt += 1
+            if temp_cnt == 1:
+                q.append([words[k], cnt+1])
+        
+    return answer
