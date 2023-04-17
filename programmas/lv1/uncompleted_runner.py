1d273351a841075a7193354de77c62d8abcb1fa0
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-04-17 10:42:43 +0900

    코딩테스트 고득점 Kit [해쉬] 완주하지 못한 선수

--- programmas/lv1/uncompleted_runner.py
+++ programmas/lv1/uncompleted_runner.py
@@ -1,3 +1,4 @@
+# 코딩테스트 고득점 Kit [해쉬] 완주하지 못한 선수
 def solution(participant, completion):
     dic = {}
     num = 0
@@ -16,3 +17,17 @@ def solution(participant, completion):
 p = ["marina", "josipa", "nikola", "vinko", "filipa"]
 c = ["josipa", "filipa", "marina", "nikola"]
 print(solution(p, c))
+
+def solution2(participant, completion):
+    player = {}
+    num = 0
+    
+    for p in participant:
+        player[hash(p)] = p
+        num += hash(p)
+    
+    for c in completion:
+        num -= hash(c)
+    
+    
+    return player[num]
