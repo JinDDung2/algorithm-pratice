2e5f75879f21558a544aa66fc06cd341d6cd971d
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-04-20 10:56:38 +0900

    코딩테스트 고득점 Kit [힙] 더 맵게

--- programmas/lv2/scoville.py
+++ programmas/lv2/scoville.py
@@ -1,4 +1,4 @@
-# 더 맵게
+# 코딩테스트 고득점 Kit [힙] 더 맵게
 
 import heapq
 
@@ -20,4 +20,22 @@ def solution(scoville, K):
         # print(f"a={a}, b={b}, hot={hot}, heap={heap}")
         answer += 1
         
+    return answer
+
+def try2(scoville, K):
+    answer = 0
+    heap = []
+    
+    for i in range(len(scoville)):
+        heapq.heappush(heap, scoville[i])
+    
+    while heap[0] < K:
+        if len(heap) == 1:
+            return -1
+        
+        answer += 1
+        fir = heapq.heappop(heap)
+        sec = heapq.heappop(heap)
+        
+        heapq.heappush(heap, fir + sec*2)
     return answer
