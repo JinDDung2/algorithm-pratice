db77dcf557ff8a42dce7cd88f6237cf05ebd6b76
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-04-21 10:26:45 +0900

    코딩테스트 고득점 Kit [정렬] 가장 큰 수

--- programmas/lv2/maximum_number.py
+++ programmas/lv2/maximum_number.py
@@ -1,5 +1,4 @@
-# 가장 큰 수
-
+# 코딩테스트 고득점 Kit [정렬] 가장 큰 수
 
 def solution(numbers):
     lst = []
@@ -8,6 +7,17 @@ def solution(numbers):
     lst.sort(key = lambda x:(x*3), reverse= True)
     answer = ''.join(lst)
     return str(int(answer))
+
+def try2(numbers):
+    answer = ''
+    
+    if sum(numbers) == 0:
+        return "0"
+    
+    lst = list(map(str, numbers))
+    lst.sort(key= lambda x:(x*3), reverse=True)
+    
+    return "".join(lst)
 
 number1 = [6, 10, 2] # "6210"
 number2 = [3, 30, 34, 5, 9] # "9534330"
