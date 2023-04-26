d1924cc52640f1decc28b0a7e72fa0dd576e67c0
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-04-26 11:29:06 +0900

    코딩테스트 고득점 Kit [완전탐색] 피로도

--- programmas/lv2/fatigability.py
+++ programmas/lv2/fatigability.py
@@ -1,5 +1,4 @@
-# 피로도
-
+# 코딩테스트 고득점 Kit [완전탐색] 피로도
 from itertools import permutations
 
 def solution(k, dungeons):
@@ -18,5 +17,21 @@ def solution(k, dungeons):
             # 지금까지의 cnt와 보다 큰 cnt가 나오면 재할당
             if answer < cnt:
                 answer = cnt
+
+def try2(k, dungeons):
+    answer = -1
+    for d in permutations(dungeons, len(dungeons)):
+        p = k
+        cnt = 0
+        for game in d:
+            if p >= game[0]:
+                p -= game[1]
+                cnt += 1
+        
+        if answer < cnt:
+            answer = cnt
+    
+    return answer
+
                 
     return answer
