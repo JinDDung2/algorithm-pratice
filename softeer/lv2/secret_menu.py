b26564b6bdc0d19e9ecb9ab15ce0a92534988b0d
jinhyuck <gjwlsgur4866@gmail.com>
2023-05-12 10:54:47 +0900

    [lv2] [21년 재직자 대회 예선] 비밀 메뉴

---
+++ softeer/lv2/secret_menu.py
@@ -0,0 +1,16 @@
+# [21년 재직자 대회 예선] 비밀 메뉴
+
+import sys
+input = sys.stdin.readline
+
+M, N, K = map(int, input().split())
+menu = list(map(str, input().split()))
+button = list(map(str, input().split()))
+
+m = ''.join(menu)
+b = ''.join(button)
+
+if m in b:
+    print("secret")
+else:
+    print("normal")
