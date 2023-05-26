ced8102556083c7f30c62f061c9e157151cec8dc
jinhyuck <gjwlsgur4866@gmail.com>
2023-05-26 10:46:50 +0900

    [G3] 소수의 연속 합

--- BOJ/two_pointer/1644.py
+++ BOJ/two_pointer/1644.py
@@ -4,49 +4,45 @@ import sys
 input = sys.stdin.readline
 num = int(input())
 
+# 에라토스테네스의 체
 def prime(num):
-    lst = []
-    if num < 2:
-        return lst
-    if num >= 2:
-        lst.append(2)
-
-    for i in range(3, num+1):
-        if isPrime(i):
-            lst.append(i)
-    
-    return lst
-
-def isPrime(num):
-    if num == 1 or num == 2:
-        return num
-    
+    check = [True for _ in range(num+1)]
     for i in range(2, int(num ** 0.5)+1):
-        if num % i == 0:
-            return False
+        if check[i] == True:
+            for j in range(i+i, num+1, i):
+                check[j] = False
+    
+    return [i for i in range(2, num+1) if check[i] == True]
 
-    return True
 
 data = prime(num)
-left = 0
-right = 0
-count = 0
-sum = data[right]
 
-while left <= right and left < len(data)-1 and right < len(data)-1:
-    if sum < num:
-        right += 1
-        sum += data[right]
-    elif sum > num:
-        sum -= data[left]
-        left += 1
+# 투포인터
+def two(data):
+    left = 0
+    right = 0
+    count = 0
+    sum = data[right]
 
-    if sum == num:
+    while left <= right and right < len(data)-1:
+        if sum < num:
+            right += 1
+            sum += data[right]
+        elif sum > num:
+            sum -= data[left]
+            left += 1
+
+        if sum == num:
+            count += 1
+            right += 1
+            sum += data[right]
+
+    if data[-1] == num:
         count += 1
-        right += 1
-        sum += data[right]
 
-if data[-1] == num:
-    count += 1
+    return count
 
-print(count)
+if num == 1:
+    print(0)
+else:
+    print(two(data))
