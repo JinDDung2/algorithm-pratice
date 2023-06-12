d8c55ee8f63a00c2af99c2ab04bdb542a0c022a3
jinhyuck <gjwlsgur4866@gmail.com>
2023-06-12 10:36:32 +0900

    만들 수 없는 금액

--- nadongbin/greedy/cant_make_money.py
+++ nadongbin/greedy/cant_make_money.py
@@ -7,6 +7,15 @@ data = list(map(int, input().split()))
 data.sort()
 
 target = 1
+for d in data:
+    if target < d:
+        break
+    target += d
+
+print(target)
+'''
+
+target = 1
 # 'target-1 까지의 모든 금액을 만들 수 있는 상태'
 for num in data:
     if target < num:
@@ -14,5 +23,7 @@ for num in data:
     target += num
 
 print(target)
+
+'''
 # 5
 # 3 2 1 1 9
