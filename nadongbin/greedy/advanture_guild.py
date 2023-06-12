ecb8f07410b21df8108ec86c441f4a0cfe2d48d8
jinhyuck <gjwlsgur4866@gmail.com>
2023-06-12 09:29:29 +0900

    모험가 길드

--- nadongbin/greedy/advanture_guild.py
+++ nadongbin/greedy/advanture_guild.py
@@ -1,21 +1,17 @@
 import sys
+
 input = sys.stdin.readline
-
-N = input()
+N = int(input().rstrip())
 data = list(map(int, input().split()))
-# print(data)
-data.sort()
 
-group = 0
-cnt = 0 # 그릅 내의 인원
+data.sort(reverse= True)
+idx = 0
+count = 0
+while idx < len(data):
+    idx += data[idx]
+    count += 1
 
-for num in data:
-    cnt += 1
-    if cnt >= num:
-        group += 1
-        cnt = 0
-
-print(f"group={group}")
+print(count)
 
 # 5
 # 2 3 1 2 2
