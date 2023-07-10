c7882306ef278684da8027283cfcef6ac876097f
jinhyuck <gjwlsgur4866@gmail.com>
2023-07-10 12:11:33 +0900

    [S3] 오리

--- BOJ/greedy_implemention/12933.py
+++ BOJ/greedy_implemention/12933.py
@@ -3,33 +3,35 @@
 import sys
 input = sys.stdin.readline
 S = input().rstrip()
-lst = []
-count = 0
-# quack
+visited = [False] * len(S)
+cnt = 0
 quack = "quack"
-# quack가 나온 적이 있으면 같은 오리
-# quack가 나온 적이 없는데 q가 있으면 다른 오리
-for s in S:
-    if s == "q" and quack not in lst:
-        lst.append("q")
-    else: # u a c k 일 경우
-        for i in range(len(lst)):
-            if len(lst[i]) == 1:
-                lst[i] += "u"
-            elif len(lst[i]) == 2:
-                lst[i] += "a"
-            elif len(lst[i]) == 3:
-                lst[i] += "c"
-            elif len(lst[i]) == 4:
-                lst[i] += "k"
-print(lst)
+# 문자열 길이가 5로 나누어떨어지지 않으면 어차피 -1
+# 방문처리용 만들기
+# 문자열에서 q를 만나면 k만날때까지 탐색 -> 방문처리 -> quack 만들어지면 flag=False(같은오리는 개수 세지 않음)
 
-for s in lst:
-    if s == "quack":
-        count += 1
+def findDuck(start):
+    global cnt
+    j = 0 # q u a c k 비교 위해
+    flag = True # 같은 오리인지 파악하기 위해
+    for i in range(start, len(S)):
+        if quack[j] == S[i] and not visited[i]:
+            visited[i] = True
+            if S[i] == "k":
+                j = 0
+                if flag:
+                    flag = False
+                    cnt += 1
+            else:
+                j += 1
 
-if count == 0 or len(S) % 5 != 0:
+if len(S) % 5 != 0:
     print(-1)
 else:
-    print(count)
-
+    for i in range(len(S)):
+        if S[i] == "q" and not visited[i]:
+            findDuck(i)
+    if cnt == 0 or not all(visited):
+        print(-1)
+    else:
+        print(cnt)
