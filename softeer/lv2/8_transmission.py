fea2357114aba4db7f93849ff38fb23db0a4cd6e
jinhyuck <gjwlsgur4866@gmail.com>
2023-07-31 10:03:56 +0900

    [lv2] 8단 변속기

--- softeer/lv2/8_transmission.py
+++ softeer/lv2/8_transmission.py
@@ -4,23 +4,41 @@ import sys
 
 input = sys.stdin.readline
 
+# test 2
 data = list(map(int, input().split()))
-result = ''
-visited = [0, 0, 0, 0, 0, 0, 0, 0]
+time = 0
 
-for i in range(1, len(data)):
-    if data[i] == data[i-1] + 1:
-        visited[i] = 0
-    elif data[i] == data[i-1] - 1:
-        visited[i] = 1
-    else:
-        visited[i] = -10
+for i in range(len(data)-1):
+    if data[i] < data[i+1]:
+        time += 1
+    elif data[i] > data[i+1]:
+        time -= 1
 
-if sum(visited) > 0:
-    result = 'descending'
-elif sum(visited) == 0:
-    result = 'ascending'
+if time == len(data)-1:
+    print("ascending")
+elif time == -(len(data)-1):
+    print("descending")
 else:
-    result = 'mixed'
+    print("mixed")
 
-print(result)
+# test 1
+# data = list(map(int, input().split()))
+# result = ''
+# visited = [0, 0, 0, 0, 0, 0, 0, 0]
+
+# for i in range(1, len(data)):
+#     if data[i] == data[i-1] + 1:
+#         visited[i] = 0
+#     elif data[i] == data[i-1] - 1:
+#         visited[i] = 1
+#     else:
+#         visited[i] = -10
+
+# if sum(visited) > 0:
+#     result = 'descending'
+# elif sum(visited) == 0:
+#     result = 'ascending'
+# else:
+#     result = 'mixed'
+
+# print(result)
