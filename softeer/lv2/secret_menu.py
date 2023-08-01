f971461f884bd57aa6655a21a91f49ae53169ab7
jinhyuck <gjwlsgur4866@gmail.com>
2023-08-01 10:45:27 +0900

    [lv2] 비밀 메뉴

--- softeer/lv2/secret_menu.py
+++ softeer/lv2/secret_menu.py
@@ -3,6 +3,18 @@
 import sys
 input = sys.stdin.readline
 
+# test
+M, N, K = map(int, input().split())
+menu = ''.join(input().rstrip())
+button = ''.join(input().rstrip())
+
+if menu in button:
+    print("secret")
+else:
+    print("normal")
+
+# test 1
+'''
 M, N, K = map(int, input().split())
 menu = list(map(str, input().split()))
 button = list(map(str, input().split()))
@@ -14,3 +26,4 @@ if m in b:
     print("secret")
 else:
     print("normal")
+'''
