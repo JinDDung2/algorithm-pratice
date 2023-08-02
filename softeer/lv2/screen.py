e3f1cfc38877021fd5c9867f0db05f3ebdc657e5
jinhyuck <gjwlsgur4866@gmail.com>
2023-08-02 19:09:46 +0900

    [21년 재직자 대회 예선] 전광판

--- softeer/lv2/screen.py
+++ softeer/lv2/screen.py
@@ -4,6 +4,42 @@ import sys
 
 input = sys.stdin.readline
 
+# test 3
+T = int(input())
+number = {
+    '0': [1, 1, 1, 1, 1, 1, 0],
+    '1': [0, 0, 0, 0, 1, 1, 0],
+    '2': [1, 0, 1, 1, 0, 1, 1],
+    '3': [1, 0, 0, 1, 1, 1, 1],
+    '4': [0, 1, 0, 0, 1, 1, 1],
+    '5': [1, 1, 0, 1, 1, 0, 1],
+    '6': [1, 1, 1, 1, 1, 0, 1],
+    '7': [1, 1, 0, 0, 1, 1, 0],
+    '8': [1, 1, 1, 1, 1, 1, 1],
+    '9': [1, 1, 0, 1, 1, 1, 1],
+    ' ': [0, 0, 0, 0, 0, 0, 0]
+}
+
+for _ in range(T):
+    A, B = input().rstrip().split()
+    result = 0
+    
+    zero_A = 5 - len(A)
+    zero_B = 5 - len(B)
+
+    A = ' ' * zero_A + A
+    B = ' ' * zero_B + B
+
+    for i in range(5):
+        if A[i] != B[i]:
+            for j in range(7):
+                if number[A[i]][j] != number[B[i]][j]:
+                    result += 1
+
+    print(result)
+
+# test 2
+'''
 num_dict = {"0" : [1, 1, 1, 1, 1, 1, 0],
             "1" : [0, 0, 1, 1, 0 ,0, 0],
             "2" : [0, 1, 1, 0, 1, 1, 1],
@@ -34,7 +70,9 @@ for i in range(T):
                 count += 1
     
     print(count)
+'''
 
+# test 1
 '''
 T = int(input())
 for i in range(T):
