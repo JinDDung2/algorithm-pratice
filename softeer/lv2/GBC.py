9ad1e9c21afd089c68f00d284b441e53e9159cfd
jinhyuck <gjwlsgur4866@gmail.com>
2023-08-02 19:37:45 +0900

    GBC

--- softeer/lv2/GBC.py
+++ softeer/lv2/GBC.py
@@ -4,6 +4,27 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
+# test 2
+test = [0] * 101
+
+start = 1
+for _ in range(N):
+    m, s = map(int, input().split())
+    for i in range(start, m+start):
+        test[i] = s - test[i]
+    start += m
+
+start = 1
+for _ in range(M):
+    m, s = map(int, input().split())
+    for i in range(start, m+start):
+        test[i] = s - test[i]
+    start += m
+
+print(max(test))
+
+# test 1
+'''
 limit_list = [0] * 101
 
 start = 1
@@ -24,3 +45,4 @@ for _ in range(M):
     start += m
 
 print(result)
+'''
