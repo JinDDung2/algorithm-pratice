596e84476554b59048096908e489346f00c22f5f
jinhyuck <gjwlsgur4866@gmail.com>
2023-08-03 11:39:25 +0900

    [v3] 징검다리

--- softeer/lv3/stepping_stone.py
+++ softeer/lv3/stepping_stone.py
@@ -6,6 +6,18 @@ input = sys.stdin.readline
 N = int(input())
 data = list(map(int, input().split()))
 
+# test 2
+dp = [1] * N
+max_num = 1
+for i in range(1, N):
+    for j in range(i):
+        if data[i] > data[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
+
+# test 1
+'''
 dp = [1] * N
 
 # 3 1 4 2 3
@@ -15,3 +27,4 @@ for i in range(1, N):
             dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
+'''
