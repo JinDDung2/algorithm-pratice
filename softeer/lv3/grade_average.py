4425be100e3153c76b4dcbdb86bd83c350b48aa9
jinhyuck <gjwlsgur4866@gmail.com>
2023-08-03 10:10:00 +0900

    [lv3] 성적 평균

--- softeer/lv3/grade_average.py
+++ softeer/lv3/grade_average.py
@@ -3,6 +3,15 @@
 import sys
 input = sys.stdin.readline
 
+# test 2
+N, K = map(int, input().split())
+data = list(map(int, input().split()))
+for i in range(K):
+    start, end = map(int, input().split())
+    average = ( sum(data[start-1:end]) / (end - start + 1) )
+    print(f"{average:.2f}")
+# test 1
+'''
 N, K = map(int, input().split())
 data = list(map(int, input().split()))
 
@@ -11,3 +20,4 @@ for _ in range(K):
     start, end = map(int, input().split())
     result = sum(data[start-1:end]) / (end-start+1)
     print(f"{result:.2f}")
+'''
