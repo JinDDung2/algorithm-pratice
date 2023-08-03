dfa1277c48bdf448de45652254a2d199d4cd3f2e
jinhyuck <gjwlsgur4866@gmail.com>
2023-08-03 10:37:29 +0900

    [lv3] 슈퍼바이러스

--- softeer/lv3/supervirus.py
+++ softeer/lv3/supervirus.py
@@ -2,7 +2,31 @@
 import sys
 
 input = sys.stdin.readline
+# test 2
+# K * P^10N
+# f(2, 10) = f(2,5) * f(2,5)
+# f(2, 5) = f(2, 2) * f(2, 2) * 2
+# f(2, 2) = f(2, 1) * f(2, 1)
+# f(2, 1) = 2
+def dfs(p, n):
+    if n == 1:
+        return p
+    elif n % 2 == 0:
+        result = dfs(p, n / 2)
+        return (result * result) % 1000000007
+    else:
+        result = dfs(p, n // 2)
+        return (result * result * p) % 1000000007
 
+
+K, P, N = map(int, input().split())
+N = 10 * N
+result = dfs(P, N)
+result = result * K % 1000000007
+print(result)
+
+# test 1
+'''
 # P^10N에서 P의 지수인 10N을 재귀적으로 나눠준다.
 def dfs(x, y):
     if y == 1:
@@ -20,3 +44,4 @@ N = 10 * N
 result = dfs(P, N)
 result *= K
 print(result % 1000000007)
+'''
