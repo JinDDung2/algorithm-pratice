f19785be3b07bde9d2deba8ab08278b4ecddaa3b
jinhyuck <gjwlsgur4866@gmail.com>
2023-08-09 09:48:26 +0900

    배열 넣기 복습

--- leetcode/merge_interval.py
+++ leetcode/merge_interval.py
@@ -17,3 +17,32 @@ class Solution:
                 result[-1][1] = max(result[-1][1], e)
         
         return result
+
+'''java
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        if (intervals.length == 1) {
+            return intervals;
+        }
+
+        List<int[]> result = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int[] cur = intervals[0];
+        for (int i=1; i<intervals.length; i++) {
+            int[] next = intervals[i];
+            // e >= next_s -> max(e, next_e)
+            if (cur[1] >= next[0]) {
+                cur[1] = Math.max(cur[1], next[1]);
+            } else {
+                result.add(cur);
+                cur = next;
+            }
+        }
+        // 나머지 1개
+        result.add(cur);
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
+'''
