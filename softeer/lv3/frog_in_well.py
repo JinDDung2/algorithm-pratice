4844f417169676bb572b6664dbba6ffa4c3777aa
jinhyuck <gjwlsgur4866@gmail.com>
2023-08-10 11:15:23 +0900

    [lv3] 우물 안 개구리

--- softeer/lv3/frog_in_well.py
+++ softeer/lv3/frog_in_well.py
@@ -6,6 +6,26 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 w = [0] + list(map(int, input().split()))
 friends = [[] for _ in range(N+1)]
+
+def isStronger(power, lst):
+    for l in lst:
+        if power <= W[l]:
+            return False
+    return True
+
+friends = [[] for _ in range(N+1)]
+result = 0
+for d in friends:
+    p1, p2 = d[0], d[1]
+    friends[p1].append(p2)
+    friends[p2].append(p1)
+
+for i in range(1, len(friends)):
+    if isStronger(W[i], friends[i]):
+        result += 1
+
+print(result)
+'''test 1
 count = 0
 
 for _ in range(M):
@@ -24,3 +44,4 @@ for i in range(1, N+1):
         count += 1
 
 print(count)
+'''
