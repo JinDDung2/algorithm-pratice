17475ae1aa38099db90c594065d57465e509fbf9
jinhyuck <gjwlsgur4866@gmail.com>
2023-08-11 11:26:29 +0900

    [lv3] 택배 마스터 광우

--- softeer/lv3/parcel_master.py
+++ softeer/lv3/parcel_master.py
@@ -1,9 +1,31 @@
 # 택배 마스터 광우
 
 import sys
-input = sys.stdin.readline
 from itertools import permutations
+input = sys.stdin.readline
 
+N, M, K = map(int, input().split())
+data = list(map(int, input().split()))
+result = sys.maxsize
+
+for p in permutations(data, N):
+    workCount = 0
+    bucket = 0
+    idx = 0
+    tempWeight = 0
+    while workCount < K:
+        if bucket + p[idx % N] <= M:
+            bucket += p[idx % N]
+            idx += 1
+        else:
+            workCount += 1
+            tempWeight += bucket
+            bucket = 0
+    result = min(result, tempWeight)
+
+print(result)
+
+'''test 1
 N, M, K = map(int, input().split())
 weights = list(map(int, input().split()))
 
@@ -31,3 +53,4 @@ for info in weights_info:
     result = min(result, temp_weight)
 
 print(result)
+'''
