f68c7e768ca268e3714deea457b150b878c87d57
jinhyuck <gjwlsgur4866@gmail.com>
2023-09-20 11:11:34 +0900

    [lv2] 짝지어 제거하기

---
+++ programmas/java/lv2/RemovePairing.java
@@ -0,0 +1,21 @@
+package programmas.java.lv2;
+
+import java.util.Stack;
+
+public class RemovePairing {
+    public int solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.empty())
+                stack.push(s.charAt(i));
+            else if (stack.peek() == s.charAt(i))
+                stack.pop();
+            else
+                stack.push(s.charAt(i));
+        }
+
+        return stack.empty() ? 1 : 0;
+    }
+}
