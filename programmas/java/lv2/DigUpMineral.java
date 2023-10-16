6d339201e3d98d5ba9e7ae8e3c0ff066deddd4c8
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-16 10:02:54 +0900

    [lv2] 광물캐기 완료

--- programmas/java/lv2/DigUpMineral.java
+++ programmas/java/lv2/DigUpMineral.java
@@ -9,18 +9,18 @@ public class DigUpMineral {
         int num = picks[0] + picks[1] + picks[2];
 
         for (int i = 0; i < minerals.length && num > 0; i++) {
-            // 곡괭이별 피로도
-            String mineral = minerals[i];
-            switch (mineral) {
+            switch (minerals[i]) {
                 case "diamond":
                     section[i / 5][0] += 1;
                     section[i / 5][1] += 5;
                     section[i / 5][2] += 25;
+                    break;
                 case "iron":
                     section[i / 5][0] += 1;
                     section[i / 5][1] += 1;
                     section[i / 5][2] += 5;
-                case "stoen":
+                    break;
+                case "stone":
                     section[i / 5][0] += 1;
                     section[i / 5][1] += 1;
                     section[i / 5][2] += 1;
@@ -33,8 +33,11 @@ public class DigUpMineral {
             System.out.println(Arrays.toString(sect));
         }
 
-        // 피로도 높은 순 내림차순
+        // 피로도 낮은 순 오름차순
         Arrays.sort(section, (o1, o2) -> (o2[2] - o1[2]));
+        for (int[] sect : section) {
+            System.out.println(Arrays.toString(sect));
+        }
 
         // 다이아 -> 철 -> 돌 순으로 캐기
         for (int i = 0, pick = 0; i < section.length; i++) {
