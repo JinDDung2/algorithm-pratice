d20acfd4d57e8d750ddb7ecfe5f9c1143ab53270
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-24 11:38:56 +0900

    189. Rotate Array 개선시키기

--- leetcode/RotateArray_189.java
+++ leetcode/RotateArray_189.java
@@ -5,7 +5,28 @@ import java.util.Deque;
 
 // 189. Rotate Array
 public class RotateArray_189 {
-    public void rotate(int[] nums, int k) {
+
+    public void rotateV2(int[] nums, int k) {
+        k %= nums.length;
+        // 배열 전부 뒤집기
+        reverse(nums, 0, nums.length - 1);
+        // 0 ~ k-1 뒤집기
+        reverse(nums, 0, k - 1);
+        // k ~ last idx 뒤집기
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public void rotateV1(int[] nums, int k) {
         Deque<Integer> deque = new ArrayDeque<>();
 
         for (int i = 0; i < nums.length; i++) {
