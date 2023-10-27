7e8594780c086e3f5bf89f2ca52ade4e7b1580b9
jinhyuck <gjwlsgur4866@gmail.com>
2023-10-27 10:33:40 +0900

    80. Remove Duplicates from Sorted Array II 복습

--- leetcode/RemoveDuplicates_80.java
+++ leetcode/RemoveDuplicates_80.java
@@ -2,7 +2,26 @@ package leetcode;
 
 // 80. Remove Duplicates from Sorted Array II
 public class RemoveDuplicates_80 {
-    public int removeDuplicates(int[] nums) {
+
+    public int removeDuplicatesV2(int[] nums) {
+
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+
+        int idx = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[idx - 2]) {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+
+        return idx;
+    }
+
+    public int removeDuplicatesV1(int[] nums) {
         if (nums.length <= 2) {
             return nums.length;
         }
