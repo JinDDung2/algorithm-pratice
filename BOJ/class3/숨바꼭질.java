18895f46d32723527279fcb06ec28344a430bcc7
jinhyuck <gjwlsgur4866@gmail.com>
2023-11-08 13:31:30 +0900

    [S1] 1697. 숨바꼭질

--- BOJ/class3/숨바꼭질.java
+++ BOJ/class3/숨바꼭질.java
@@ -8,8 +8,6 @@ public class 숨바꼭질 {
     private static int N;
     private static int K;
     private static int[] dp;
-    private static boolean[] visited;
-    private static Queue<Integer> que;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,26 +16,30 @@ public class 숨바꼭질 {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        dp = new int[K + 1];
-        visited = new boolean[K + 1];
-        que = new LinkedList<>();
-        dp[N] = 1;
+        dp = new int[100001];
+        Arrays.fill(dp, -1);
+        int result = bfs();
+        System.out.println(result);
+    }
+
+    private static int bfs() {
+        Queue<Integer> que = new LinkedList<>();
+        dp[N] = 0;
         que.add(N);
 
         while (true) {
-            int idx = que.poll();
-            if (idx == K) {
-                System.out.println(dp[K]);
-                break;
+            int curr = que.poll();
+            if (curr == K) {
+                return dp[K];
             }
 
-            int[] idxs = new int[] { idx - 1, idx + 1, idx * 2 };
-            for (int next : idxs) {
-                if (next < 0 || next >= K + 1 || visited[next])
-                    continue;
-                que.add(next);
-                visited[next] = true;
-                dp[next] = dp[idx] + 1;
+            int[] nextIdxs = new int[] { curr - 1, curr + 1, curr * 2 };
+            for (int next : nextIdxs) {
+                if (next >= 0 && next <= 100000 && dp[next] == -1) {
+                    dp[next] = dp[curr] + 1;
+                    que.add(next);
+
+                }
             }
         }
     }
