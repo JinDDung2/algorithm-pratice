c0af06760ffc2c222694e6bd01fe8303aed1ea86
jinhyuck <gjwlsgur4866@gmail.com>
2023-12-01 13:17:11 +0900

    1283. Find the Smallest Divisor Given a Threshold 리팩토링 12ms -> 5ms

--- leetcode/FindSmallestDivisorGivenThreshold_1283.java
+++ leetcode/FindSmallestDivisorGivenThreshold_1283.java
@@ -2,7 +2,7 @@ package leetcode;
 
 // 1283. Find the Smallest Divisor Given a Threshold
 public class FindSmallestDivisorGivenThreshold_1283 {
-    public int smallestDivisor(int[] nums, int threshold) {
+    public int smallestDivisorV1(int[] nums, int threshold) {
         int left = 1, right = (int) Math.pow(10, 6);
 
         while (left < right) {
@@ -22,5 +22,31 @@ public class FindSmallestDivisorGivenThreshold_1283 {
         }
 
         return left;
+    }
+
+    public int smallestDivisorV2(int[] nums, int threshold) {
+        int left = 1, right = (int) Math.pow(10, 6);
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (helper(nums, mid) <= threshold) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private int helper(int[] nums, int divisor) {
+        int sumOfDivide = 0;
+        for (int num : nums) {
+            // sumOfDivide += num / divisor;
+            // sumOfDivide += (num % divisor != 0 ? 1 : 0);
+            sumOfDivide += (num + divisor - 1) / divisor;
+        }
+        return sumOfDivide;
     }
 }
