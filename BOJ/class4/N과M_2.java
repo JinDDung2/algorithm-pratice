54be409bf62a34926d61ae887b22c1fdf40efed0
jinhyuck <gjwlsgur4866@gmail.com>
2023-12-03 15:44:35 +0900

    [S3] 15650. N과 M (2)

--- BOJ/class4/N과M_2.java
+++ BOJ/class4/N과M_2.java
@@ -1,8 +1,25 @@
 package BOJ.class4;
 
+import java.io.*;
+import java.util.*;
+
 // [S3] 15650. N과 M (2)
 public class N과M_2 {
-    public static void main(String[] args) {
+    static int N;
+    static int M;
+    static int[] arr;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = i;
+        }
     }
 }
