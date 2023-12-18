ab71684e3adc18ee0ed7326a3b82575e896502cd
jinhyuck <gjwlsgur4866@gmail.com>
2023-12-18 14:08:49 +0900

    [G5] 2470. 두 용액

---
+++ BOJ/two_pointer/두용액_2470.java
@@ -0,0 +1,40 @@
+package BOJ.two_pointer;
+
+import java.io.*;
+import java.util.*;
+
+// [G5] 2470. 두 용액
+public class 두용액_2470 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] data = new int[N];
+        for (int i = 0; i < N; i++) {
+            data[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(data);
+        int l = 0, r = N - 1;
+        int minSum = Math.abs(data[l] + data[r]);
+        int[] result = { data[l], data[r] };
+
+        while (l < r) {
+            int sum = data[l] + data[r];
+            if (minSum > Math.abs(sum)) {
+                minSum = Math.abs(sum);
+                result[0] = data[l];
+                result[1] = data[r];
+                if (sum == 0)
+                    break;
+            }
+
+            if (sum > 0)
+                r--;
+            else
+                l++;
+        }
+
+        System.out.printf("%d %d", result[0], result[1]);
+        br.close();
+    }
+}
