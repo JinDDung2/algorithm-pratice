766fd44404720daf40725fd6a3702187b0913b14
jinhyuck <gjwlsgur4866@gmail.com>
2024-01-12 08:59:41 +0900

    141. Linked List Cycle

---
+++ leetcode/LinkedListCycle_141.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+
+// 141. Linked List Cycle
+public class LinkedListCycle_141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
