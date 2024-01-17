6c5625e2ab641a6363db17de8cb79e8e0f4fc8f6
jinhyuck <gjwlsgur4866@gmail.com>
2024-01-17 16:14:00 +0900

    2018 카카오 블라인드 채용 비밀지도

---
+++ programmas/kakao/비밀지도.java
@@ -0,0 +1,16 @@
+package programmas.kakao;
+
+public class 비밀지도 {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            answer[i] = String.format("%" + n + "s", answer[i]);
+            answer[i] = answer[i].replaceAll("1", "#");
+            answer[i] = answer[i].replaceAll("0", " ");
+        }
+
+        return answer;
+    }
+}
