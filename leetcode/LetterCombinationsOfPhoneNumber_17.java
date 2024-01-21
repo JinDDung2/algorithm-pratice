e9294792fc96b1d83947077f35f2dc5cba9ebdbc
jinhyuck <gjwlsgur4866@gmail.com>
2024-01-21 10:38:26 +0900

    17. Letter Combinations of a Phone Number

--- leetcode/LetterCombinationsOfPhoneNumber_17.java
+++ leetcode/LetterCombinationsOfPhoneNumber_17.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// 17. Letter Combinations of a Phone Number
 public class LetterCombinationsOfPhoneNumber_17 {
     Map<Character, String> map = new HashMap<>();
 
@@ -36,6 +37,33 @@ public class LetterCombinationsOfPhoneNumber_17 {
         char[] arr = map.get(digits.charAt(idx)).toCharArray();
         for (int i = 0; i < arr.length; i++) {
             find(result, digits, idx + 1, word + arr[i]);
+        }
+    }
+
+    public List<String> letterCombinationsV2(String digits) {
+        List<String> result = new ArrayList<>();
+
+        if (digits.length() == 0) {
+            return result;
+        }
+
+        String[] letters = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        backtrack(0, new StringBuilder(), result, digits, letters);
+        return result;
+    }
+
+    private void backtrack(int idx, StringBuilder sb, List<String> result, String digits, String[] letters) {
+        if (idx == digits.length()) {
+            result.add(sb.toString());
+            return;
+        }
+
+        int num = digits.charAt(idx) - '0';
+        String str = letters[num];
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(str.charAt(i));
+            backtrack(idx + 1, sb, result, digits, letters);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 
