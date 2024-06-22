d06e798b6acecba87f7033da8575a1358cf517d6
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-06-22 18:06:47 +0900

    Stop tracking out directory

--- out/production/algorithm-pratice/README.md
+++
@@ -1,2 +0,0 @@
-# algorithm-pratice
-알고리즘 문제풀기 공간
--- out/production/algorithm-pratice/programmas/python/lv1/kim.py
+++
@@ -1,9 +0,0 @@
-# 서울에서 김서방찾기
-
-def solution(seoul):
-    for i in range(len(seoul)):
-        if seoul[i] == "Kim":
-            return f"김서방은 {i}에 있다"
-    
-
-print(solution(["Jane", "Kim"]))
