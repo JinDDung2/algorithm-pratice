8422346c4804126a4d7a01061cbacb67b2c3c6c7
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-07-05 17:32:15 +0900

    update .gitignore to ignore specific directories

--- out/production/algorithm-pratice/programmas/python/lv1/not_sameNumber.py
+++
@@ -1,9 +0,0 @@
-# 코딩테스트 고득점 Kit [스택/큐] 같은 숫자는 싫어
-def solution(arr):
-    answer = []
-    answer.append(arr[0])
-    
-    for i in range(1, len(arr)):
-        if arr[i] != answer[-1]: answer.append(arr[i]) 
-    
-    return answer
