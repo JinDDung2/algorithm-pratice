da536bbe8b7339e264f9ff49618cdc1516de3052
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-07-06 19:36:02 +0900

    1657. Determine if Two Strings Are Close

--- leetcode/Determine_if_Two_Strings_Are_Close_1657.java
+++ leetcode/Determine_if_Two_Strings_Are_Close_1657.java
@@ -3,8 +3,6 @@ package leetcode;
 import java.util.Arrays;
 
 public class Determine_if_Two_Strings_Are_Close_1657 {
-    // if (w1[i] > 0 && w2[i] == 0) || (w1[i] == 0 && w2[i] > 0) -> false
-    // 같은 개수가 있어야 알파벳들을 한꺼번에 변경 가능
     public boolean closeStrings(String word1, String word2) {
         int[] w1 = new int[26];
         int[] w2 = new int[26];
