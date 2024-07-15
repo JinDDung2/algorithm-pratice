052b518d72d386785fee342fe0a895fd50b6c356
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-07-15 10:47:20 +0900

    1503. Last Moment Before All Ants Fall Out of a Plank

---
+++ leetcode/Last_Moment_Before_All_Ants_Fall_Out_of_a_Plank_1503.java
@@ -0,0 +1,15 @@
+class Last_Moment_Before_All_Ants_Fall_Out_of_a_Plank_1503 {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int max = 0;
+
+        for (int l : left) {
+            max = Math.max(max, l);
+        }
+
+        for (int r : right) {
+            max = Math.max(max, n - r);
+        }
+
+        return max;
+    }
+}
