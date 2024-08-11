b2573b11e66576b884c9198b69722947cda53897
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2024-08-11 17:04:56 +0900

    [lv3] 섬 연결하기

---
+++ programmas/java/lv3/섬연결하기.java
@@ -0,0 +1,43 @@
+package programmas.java.lv3;
+
+import java.util.Arrays;
+
+// [lv3] 섬 연결하기
+public class 섬연결하기 {
+
+    int[] parent;
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+
+        // 간선 비용을 오름차순으로 정렬
+        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
+
+        // 유니온-파인드 배열 초기화
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        return answer;
+    }
+
+    // 유니온 함수
+    private void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootX] = rootY;
+        }
+    }
+
+    // 파인드 함수
+    private int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x]); // 경로 압축
+    }
+
+}
+
+
