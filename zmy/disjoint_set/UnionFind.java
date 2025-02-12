3965c3a604c1add42cf25f8cd3626a33f477fd5b
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-02-12 10:37:29 +0900

    union&find 최적화

--- zmy/disjoint_set/UnionFind.java
+++ zmy/disjoint_set/UnionFind.java
@@ -4,6 +4,7 @@ public class UnionFind {
 
     static final int MAX_SIZE = 1_000;
     int[] parent = new int[MAX_SIZE];
+    int[] rank = new int[MAX_SIZE];
 
     int[] solution() {
         for (int i = 0; i < MAX_SIZE; i++)
@@ -36,6 +37,42 @@ public class UnionFind {
         y = find(y);
 
         parent[y] = x;
+    }
+
+    /**
+     * 시간복잡도: O(logN)
+     */
+    void unionByRank(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+
+    int unionForCountTotalNodes(int x, int y) {
+        int[] nodeCount = new int[MAX_SIZE];
+        for (int i = 0; i < MAX_SIZE; i++) {
+            nodeCount[i] = 1;
+        }
+
+        x = find(x);
+        y = find(y);
+
+        // 두 값의 root가 같지 않으면
+        if(x != y) {
+            parent[y] = x; // y의 root를 x로 변경
+            nodeCount[x] += nodeCount[y]; // x의 node 수에 y의 node 수를 더한다.
+            nodeCount[y] = 1; // x에 붙은 y의 node 수는 1로 초기화
+        }
+        return nodeCount[x]; // 가장 root의 node 수 반환
     }
 
 }
