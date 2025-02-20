6683a8f6d47ee4217c036501efd686475dbab95b
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-02-20 18:32:31 +0900

    combination 2

--- zmy/dfs/Combination.java
+++ zmy/dfs/Combination.java
@@ -5,31 +5,58 @@ import java.util.List;
 
 public class Combination {
 
-    public List<List<Integer>> generateCombinations(int[] nums, int k) {
+    List<List<Integer>> generateCombinations(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, k, 0, new ArrayList<>(), result);
+        backtrackV1(nums, 0, new ArrayList<>(), result);
 
         return result;
     }
 
-    private void backtrack(int[] nums, int k, int start, List<Integer> current, List<List<Integer>> result) {
-        if (current.size() == k) {
-            result.add(new ArrayList<>(current));
+    void backtrackV1(int[] nums, int start, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() > nums.length) {
             return;
+        }
+
+        result.add(new ArrayList<>(current));
+
+        for (int i = start; i < nums.length; i++) {
+            current.add(nums[i]); // 선택
+            backtrackV1(nums, i + 1, current, result); // 다음 단계 탐색
+            current.remove(current.size() - 1); // 백트래킹 (원상 복구)
+        }
+    }
+
+    List<List<Integer>> findCombinationsWithTarget(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrackV2(nums, 0, new ArrayList<>(), result, target, 0);
+        return result;
+    }
+
+    void backtrackV2(int[] nums, int start, List<Integer> current, List<List<Integer>> result, int target, int sum) {
+        if (current.size() > nums.length) {
+            return;
+        }
+
+        if (sum == target) { // target과 같은 경우만 저장
+            result.add(new ArrayList<>(current));
         }
 
         for (int i = start; i < nums.length; i++) {
             current.add(nums[i]); // 선택
-            backtrack(nums, k, i + 1, current, result); // 다음 단계 탐색
+            backtrackV2(nums, i + 1, current, result, target, sum + nums[i]); // 합 업데이트
             current.remove(current.size() - 1); // 백트래킹 (원상 복구)
         }
     }
 
     public static void main(String[] args) {
         Combination p = new Combination();
-        int[] nums = {1, 2, 3, 4}; // 입력 배열
-        int k = 2; // 조합 크기
-        List<List<Integer>> combinations = p.generateCombinations(nums, k);
-        System.out.println(combinations); // [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+        int[] nums1 = {1, 2, 3, 4}; // 입력 배열
+        List<List<Integer>> combinations = p.generateCombinations(nums1);
+        System.out.println(combinations);
+
+        int[] nums2 = {1, 2, 3, 4, 5}; // 입력 배열
+        int target = 7; // 목표 합
+        List<List<Integer>> combinationsWithTarget = p.findCombinationsWithTarget(nums2, target);
+        System.out.println(combinationsWithTarget); // [[1, 2, 4], [2, 5], [3, 4]]
     }
 }
