001f379d24ea28ac841419b4c2c788003f18ec99
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-02-25 17:19:44 +0900

    [P5] 6549. 히스토그램에서 가장 큰 직사각형 미해결

---
+++ BOJ/P6549_히스토그램에서가장큰직사각형.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+public class P6549_히스토그램에서가장큰직사각형 {
+    static class Problem {
+        String solution() {
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem p = new Problem();
+        System.out.println(p.solution());
+    }
+}
