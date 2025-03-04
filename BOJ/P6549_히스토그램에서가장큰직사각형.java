e94762fb0b9635ebb4f33768d5bdc0f2f3d1a53d
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-03-04 19:37:19 +0900

    [P5] 6549. 히스토그램에서 가장 큰 직사각형 segment tree

--- BOJ/P6549_히스토그램에서가장큰직사각형.java
+++ BOJ/P6549_히스토그램에서가장큰직사각형.java
@@ -49,14 +49,83 @@ public class P6549_히스토그램에서가장큰직사각형 {
 
             return maxArea;
         }
+
+        void solutionV2() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            String input;
+
+            while (!(input = br.readLine()).equals("0")) {
+                StringTokenizer st = new StringTokenizer(input);
+                int N = Integer.parseInt(st.nextToken());
+                long[] heights = new long[N];
+                for (int i = 0; i < N; i++) {
+                    heights[i] = Long.parseLong(st.nextToken());
+                }
+
+                SegmentTree segmentTree = new SegmentTree(heights.length);
+                segmentTree.init(heights, 1, 0, heights.length - 1);
+                bw.write(segmentTree.getMaxArea(heights, 0, N - 1) + "\n");
+            }
+
+            bw.flush();
+            bw.close();
+            br.close();
+        }
     }
 
-    String solutionV2() {
-        return "";
+    static class SegmentTree {
+        int[] tree;
+        int treeSize;
+
+        public SegmentTree(int arrSize) {
+            int h = (int) Math.ceil(Math.log(arrSize) / Math.log(2));
+            this.treeSize = (int) Math.pow(2, h + 1);
+            tree = new int[treeSize];
+        }
+
+        int init(long[] heights, int node, int start, int end) {
+            if (start == end) return tree[node] = start;
+
+            int mid = (start + end) / 2;
+            int leftIdx = init(heights, node * 2, start, mid);
+            int rightIdx = init(heights, node * 2 + 1, mid + 1, end);
+
+            return tree[node] = (heights[leftIdx] <= heights[rightIdx]) ? leftIdx : rightIdx;
+        }
+
+        // 구간 내 최소 높이 인덱스 찾기
+        int query(long[] heights, int node, int start, int end, int left, int right) {
+            if (left > end || right < start) return -1;
+            if (left <= start && end <= right) return tree[node];
+
+            int mid = (start + end) / 2;
+            int leftIdx = query(heights, node * 2, start, mid, left, right);
+            int rightIdx = query(heights, node * 2 + 1, mid + 1, end, left, right);
+
+            if (leftIdx == -1) return rightIdx;
+            if (rightIdx == -1) return leftIdx;
+            return (heights[leftIdx] <= heights[rightIdx]) ? leftIdx : rightIdx;
+        }
+
+        long getMaxArea(long[] heights, int left, int right) {
+            if (left > right) return 0;
+
+            int minIdx = query(heights, 1, 0, heights.length - 1, left, right);
+            long area = heights[minIdx] * (right - left + 1);
+
+            long leftArea = getMaxArea(heights, left, minIdx - 1);
+            long rightArea = getMaxArea(heights, minIdx + 1, right);
+
+            return Math.max(
+                    area, Math.max(leftArea, rightArea)
+            );
+        }
     }
 
     public static void main(String[] args) throws IOException {
         Problem p = new Problem();
-        System.out.println(p.solution());
+//        System.out.println(p.solution());
+        p.solutionV2();
     }
 }
