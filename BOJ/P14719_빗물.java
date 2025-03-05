9c626c62860418cf401146be26919bfefd14bb5f
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-03-05 18:08:01 +0900

    [G5] 14719. 빗물

---
+++ BOJ/P14719_빗물.java
@@ -0,0 +1,13 @@
+package BOJ;
+
+public class P14719_빗물 {
+    static class Problem {
+        int solution() {
+            return 0;
+        };
+    }
+    public static void main(String[] args) {
+        Problem problem = new Problem();
+        System.out.println(problem.solution());
+    }
+}
