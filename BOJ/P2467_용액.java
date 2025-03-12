240e56b13a191675b9b03939a5d3cdbf4fe29adc
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-03-12 12:39:28 +0900

    [G5] 2467. 용액

--- BOJ/P2467_용액.java
+++ BOJ/P2467_용액.java
@@ -8,74 +8,46 @@ public class P2467_용액 {
     static class Problem {
 
         /**
-         * 여러개의 노드와 각각의 간선에 대한 비용이 존재.
-         * 최소한의 비용으로 목적지에 도달 -> 다익스트라
+         * 두 값의 합이 0과 가장 가까운 두 인덱스를 구하기
+         * N <= 100,000 -> 이중 For문 시간초과
+         * 투포인터
+         * N개의 용액들의 특성값은 모두 서로 다름 == 중복값 없음
+         * 특성값이 0에 가장 가까운 용액을 만들어내는 경우가 두 개 이상일 경우에는 그 중 아무것이나 하나를 출력
          */
         void solution() throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()); // <= 50,000
-            int M = Integer.parseInt(st.nextToken()); // <= 50,000
+            int N = Integer.parseInt(br.readLine()); // <= 100,000
+            int[] arr = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
 
-            List<List<Node>> graph = new ArrayList<>();
-            for (int i = 0; i <= N; i++) {
-                graph.add(new ArrayList<>());
-            }
-            for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-                int from = Integer.parseInt(st.nextToken());
-                int to = Integer.parseInt(st.nextToken());
-                int cost = Integer.parseInt(st.nextToken());
-                graph.get(from).add(new Node(to, cost));
-                graph.get(to).add(new Node(from, cost));
+            // 두 값이 0과 비교할 target
+            // 출력값은 인덱스가 아닌 두 값.
+            int l = 0, r = arr.length - 1;
+            int[] result = new int[2]; // 두 값을 담을 배열
+            int minSum = Integer.MAX_VALUE;
+
+            while (l < r) {
+                int sum = arr[l] + arr[r];
+
+                if (minSum > Math.abs(sum)) {
+                    minSum = Math.abs(sum);
+                    result[0] = arr[l];
+                    result[1] = arr[r];
+                }
+
+                if (sum == 0) break;
+                else if (sum > 0) r--;
+                else l++;
             }
 
-            // 1에서 시작
-            // 가지 못하는 경우 X
-            int[] distances = dijkstra(1, graph);
-            bw.write(String.valueOf(distances[N]));
+            bw.write(result[0] + " " + result[1]);
             br.close();
             bw.flush();
             bw.close();
         }
 
-        int[] dijkstra(int start, List<List<Node>> graph) {
-            int[] distances = new int[graph.size()];
-            Arrays.fill(distances, Integer.MAX_VALUE);
-
-            PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n1.cost, n2.cost));
-            distances[start] = 0;
-            pq.offer(new Node(1, 0));
-
-            while(!pq.isEmpty()) {
-                Node curr = pq.poll();
-                int currIdx = curr.index;
-                int currCost = curr.cost;
-
-                if (currCost > distances[currIdx]) continue;
-
-                for (Node next : graph.get(currIdx)) {
-                    int cost = currCost + next.cost;
-                    if (cost < distances[next.index]) {
-                        distances[next.index] = cost;
-                        pq.offer(new Node(next.index, cost));
-                    }
-                }
-            }
-
-            return distances;
-        }
-    }
-
-    static class Node {
-        int index;
-        int cost;
-
-        public Node(int index, int cost) {
-            this.index = index;
-            this.cost = cost;
-        }
     }
 
     public static void main(String[] args) throws IOException {
