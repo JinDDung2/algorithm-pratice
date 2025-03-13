d280f7551cee468178d32db098f6afc576d8e25d
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-03-13 17:22:17 +0900

    [G5] 7682. 틱택토

---
+++ BOJ/P7682_틱택토.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+public class P7682_틱택토 {
+    static class Problem {
+        void solution() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem problem = new Problem();
+        problem.solution();
+    }
+}
