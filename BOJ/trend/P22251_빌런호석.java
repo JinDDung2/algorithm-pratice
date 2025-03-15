b8bade8d6ceb02346f40dde94a10e2f8459d2f8d
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-03-15 19:53:46 +0900

    [G5] 22251. 빌런 호석

---
+++ BOJ/trend/P22251_빌런호석.java
@@ -0,0 +1,15 @@
+package BOJ.trend;
+
+public class P22251_빌런호석 {
+    static class Problem {
+        void solution() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem problem = new Problem();
+        problem.solution();
+    }
+
+}
