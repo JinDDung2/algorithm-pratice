a50b41b41ccd11c7f6014d4b218a50ef6e7bdd8b
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-03-20 22:31:31 +0900

    [G4] 7490. 인구 이동

---
+++ BOJ/P7490_인구이동.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+public class P7490_인구이동 {
+    static class Problem {
+        void solution() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem problem = new Problem();
+        problem.solution();
+    }
+}
