6431fc2374bb7790cec9723aa6eca7085fb79ffa
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-05-09 11:29:21 +0900

    [G4] 2293. 동전 top-down, bottom-up

--- BOJ/길라잡이/dp/P2293_동전.java
+++ BOJ/길라잡이/dp/P2293_동전.java
@@ -1,15 +1,76 @@
 package BOJ.길라잡이.dp;
 
+import java.io.*;
+import java.util.*;
+
 public class P2293_동전 {
 
     static class Problem {
-        void solution() {
 
+        int N, K;
+        int[] coins;
+        int[] dp;
+        int[][] memo;
+
+        void solutionWithBottomUp() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            coins = new int[N];
+            for (int i = 0; i < N; i++) {
+                coins[i] = Integer.parseInt(br.readLine());
+            }
+
+            dp = new int[K + 1];
+            dp[0] = 1;
+
+            for (int coin : coins) {
+                for (int i = coin; i <= K; i++) {
+                    dp[i] += dp[i - coin];
+                }
+            }
+
+            System.out.println(dp[K]);
+        }
+
+        void solutionWithTopDown() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            coins = new int[N];
+            for (int i = 0; i < N; i++) {
+                coins[i] = Integer.parseInt(br.readLine());
+            }
+
+            memo = new int[N + 1][K + 1];
+            for (int[] m : memo) {
+                Arrays.fill(m, -1);
+            }
+
+            int result = solve(0, 0);
+            System.out.println(result);
+        }
+
+        int solve(int idx, int sum) {
+            if (sum == K) return 1;
+            if (idx >= N || sum > K) return 0;
+
+            if (memo[idx][sum] != -1) return memo[idx][sum];
+
+            int skip = solve(idx + 1, sum);
+            int use = solve(idx, sum + coins[idx]);
+
+            return memo[idx][sum] = use + skip;
         }
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         Problem problem = new Problem();
-        problem.solution();
+//        problem.solutionWithBottomUp();
+        System.out.println("------");
+        problem.solutionWithTopDown();
     }
 }
