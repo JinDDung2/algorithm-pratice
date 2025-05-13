32dc8403f4e8c4ecc3cc88a9eb587f53e5b32296
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-05-13 13:48:51 +0900

    [G3] 11066. 파일 합치기

---
+++ BOJ/길라잡이/dp/P11066_파일합치기.java
@@ -0,0 +1,59 @@
+package BOJ.길라잡이.dp;
+
+import java.io.*;
+import java.util.*;
+
+public class P11066_파일합치기 {
+
+    static class Problem {
+
+        int T, K;
+        int[] files;
+        int[] prefixSum;
+        int[][] dp;
+
+        void solution() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            T = Integer.parseInt(br.readLine());
+
+            while (T-- > 0) {
+                K = Integer.parseInt(br.readLine());
+                files = new int[K + 1];
+                prefixSum = new int[K + 1];
+                dp = new int[K + 1][K + 1];
+
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int i = 1; i <= K; i++) {
+                    files[i] = Integer.parseInt(st.nextToken());
+                    prefixSum[i] = prefixSum[i - 1] + files[i];
+                }
+
+                for (int len = 2; len <= K; len++) { // 구간 길이
+                    for (int i = 1; i <= K - len + 1; i++) {
+                        int j = i + len - 1;
+                        dp[i][j] = Integer.MAX_VALUE;
+
+                        for (int k = i; k < j; k++) { // 병합 나누는 지점
+                            // i~k 합친 비용 + k+1~j 합친 비용 + 전체 구간 파일 크기(누적합 활용)
+                            int cost = dp[i][k] + dp[k + 1][j] + prefixSum[j] - prefixSum[i - 1];
+                            dp[i][j] = Math.min(dp[i][j], cost);
+                        }
+                    }
+                }
+
+                bw.write(dp[1][K] + "\n");
+            }
+
+            bw.flush();
+            bw.close();
+            br.close();
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Problem problem = new Problem();
+        problem.solution();
+    }
+}
