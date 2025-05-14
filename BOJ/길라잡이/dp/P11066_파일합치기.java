3bbe4940e90f55ec8e0ed08acaf6e44fae6c6c1e
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2025-05-14 14:37:15 +0900

    [G3] 11066. 파일 합치기

--- BOJ/길라잡이/dp/P11066_파일합치기.java
+++ BOJ/길라잡이/dp/P11066_파일합치기.java
@@ -12,7 +12,7 @@ public class P11066_파일합치기 {
         int[] prefixSum;
         int[][] dp;
 
-        void solution() throws IOException {
+        void solutionWithBottomUp() throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
             T = Integer.parseInt(br.readLine());
@@ -50,10 +50,54 @@ public class P11066_파일합치기 {
             br.close();
         }
 
+        void solutionWithTopDown() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            T = Integer.parseInt(br.readLine());
+
+            while (T-- > 0) {
+                K = Integer.parseInt(br.readLine());
+                files = new int[K + 1];
+                prefixSum = new int[K + 1];
+                dp = new int[K + 1][K + 1];
+
+                for (int[] r : dp) {
+                    Arrays.fill(r, -1);
+                }
+
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int i = 1; i <= K; i++) {
+                    files[i] = Integer.parseInt(st.nextToken());
+                    prefixSum[i] = prefixSum[i - 1] + files[i];
+                }
+
+                int result = solve(1, K);
+                bw.write(result + "\n");
+            }
+
+            bw.flush();
+            bw.close();
+            br.close();
+        }
+
+        int solve(int i, int j) {
+            if (i == j) return 0;
+            if (dp[i][j] != -1) return dp[i][j];
+
+            dp[i][j] = Integer.MAX_VALUE;
+
+            for (int k = i; k < j; k++) {
+                int cost = solve(i, k) + solve(k + 1, j) + prefixSum[j] - prefixSum[i - 1];
+                dp[i][j] = Math.min(dp[i][j], cost);
+            }
+
+            return dp[i][j];
+        }
     }
 
     public static void main(String[] args) throws IOException {
         Problem problem = new Problem();
-        problem.solution();
+//        problem.solutionWithBottomUp();
+        problem.solutionWithTopDown();
     }
 }
