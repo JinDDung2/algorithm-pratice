49853d8ce1ea93d66f08248f240bfb81e5fd4280
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2026-01-03 13:57:14 +0900

    [lv2] 2025 프로그래머스 코드챌린지 2차 예선 - 서버 증설 횟수

--- programmas/java/lv2/NumberOfServerExtensions.java
+++ programmas/java/lv2/NumberOfServerExtensions.java
@@ -28,4 +28,23 @@ public class NumberOfServerExtensions {
         return answer;
     }
 
+    public int anotherSolution(int[] players, int m, int k) {
+        int ans = 0;
+        int active = 0;
+        int[] install = new int[24];
+
+        for (int i = 0; i < 24; i++) {
+            if (i >= k) {
+                active -= install[i - k];
+            }
+            int req = players[i] / m;
+            if (active < req) {
+                int add = req - active;
+                ans += add;
+                active += add;
+                install[i] = add;
+            }
+        }
+        return ans;
+    }
 }
