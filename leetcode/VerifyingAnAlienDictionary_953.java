5866dee0f9ab59c5aa1788bfb9b6dc7a04b8f8ae
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2026-03-02 14:54:03 +0900

    953. Verifying an Alien Dictionary

--- leetcode/VerifyingAnAlienDictionary_953.java
+++ leetcode/VerifyingAnAlienDictionary_953.java
@@ -6,6 +6,45 @@ import java.util.Map;
 // 953. Verifying an Alien Dictionary
 public class VerifyingAnAlienDictionary_953 {
 
+    public boolean isAlienSortedV3(String[] words, String order) {
+
+        if (words == null || words.length == 1) return true;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < order.length(); i++) {
+            map.put(order.charAt(i), i);
+        }
+
+        String prev = words[0];
+        for (int i = 1; i < words.length; i++) {
+            boolean allSame = true;
+            String cur = words[i];
+
+            for (int j = 0; j < Math.min(prev.length(), cur.length()); j++) {
+                char p = prev.charAt(j);
+                char c = cur.charAt(j);
+
+                if (p != c) {
+                    allSame = false;
+                    if (map.get(p) > map.get(c)) {
+                        return false;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            if (allSame && prev.length() > cur.length()) {
+                return false;
+            }
+
+            prev = cur;
+        }
+
+        return true;
+    }
+
     public boolean isAlienSortedV2(String[] words, String order) {
 
         if (words == null || words.length == 1) {
