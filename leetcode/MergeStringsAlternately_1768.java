827b40ffd87b1803f77dc03b5a62c86682d6fbb4
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2026-03-12 06:29:35 +0900

    1768. Merge Strings Alternately

---
+++ leetcode/MergeStringsAlternately_1768.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+// 1768. Merge Strings Alternately
+public class MergeStringsAlternately_1768 {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+        int j = 0;
+
+        while (i < word1.length() && j < word2.length()) {
+            sb.append(word1.charAt(i++));
+            sb.append(word2.charAt(j++));
+        }
+
+        while (i < word1.length()) {
+            sb.append(word1.charAt(i++));
+        }
+
+        while (j < word2.length()) {
+            sb.append(word2.charAt(j++));
+        }
+
+        return sb.toString();
+    }
+}
