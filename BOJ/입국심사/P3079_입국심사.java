d2d909d19b87b91401dd9e6c5ee4a571d01610ce
Jin-hyuck <92037996+JinDDung2@users.noreply.github.com>
2026-04-04 14:22:56 +0900

    3079. 입국심사

--- BOJ/입국심사/P3079_입국심사.java
+++ BOJ/입국심사/P3079_입국심사.java
@@ -1,4 +1,4 @@
-package BOJ.길라잡이.parametric;
+package BOJ.입국심사;
 
 import java.io.*;
 import java.util.*;
@@ -25,13 +25,19 @@ public class P3079_입국심사 {
             int maxTime = times[times.length - 1];
             long l = 1, r = (long) M * maxTime;
             long result = r;
-            while (l <= r) {
+            while (l < r) {
                 long m = l + (r - l) / 2;
-                long count = calculate(m);
+                long sum = 0;
 
-                if (count >= M) {
+                for (long t : times) {
+                    sum += m / t;
+
+                    if (sum >= M) break;
+                }
+
+                if (sum >= M) {
                     result = m;
-                    r = m - 1;
+                    r = m;
                 } else {
                     l = m + 1;
                 }
@@ -42,21 +48,6 @@ public class P3079_입국심사 {
             bw.close();
             br.close();
         }
-
-        long calculate(long m) {
-            long count = 0;
-
-            for (int t : times) {
-                count += m / t;
-            }
-
-            if (count >= M) {
-                return M;
-            }
-
-            return count;
-        }
-
     }
 
     public static void main(String[] args) throws IOException {
